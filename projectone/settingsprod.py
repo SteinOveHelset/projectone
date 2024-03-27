199fbd4f05d4b51c5a580c2b6da75dc6200acb32
Stein Ove Helset <steinovehelset@gmail.com>
2024-03-27 19:14:01 +0000

    changed hosts

--- projectone/settingsprod.py
+++ projectone/settingsprod.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['projectone.codewithstein.com']
 
 
 # Application definition
